1a881e36a07ab264ea1a82694b8808adbbacbbd0
Ezequiel Brito Navarro <ebritonavarro@gmail.com>
2025-12-30 12:43:35 -0500

    chore: mock IntersectionObserver for testing purposes

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,17 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+
+class IntersectionObserverMock {
+  constructor(callback) {
+    this.callback = callback;
+  }
+  observe = vi.fn();
+  unobserve = vi.fn();
+  disconnect = vi.fn();
+}
+
+Object.defineProperty(window, 'IntersectionObserver', {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserverMock,
+});

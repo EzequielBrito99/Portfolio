7c8be27fa1118c84deeb39dc25a25ede2dcfcce9
Ezequiel Brito Navarro <ebritonavarro@gmail.com>
2026-01-03 12:10:46 -0500

    chore: setup lighthouse

---
+++ lighthouserc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'pnpm run preview',
+      url: ['http://localhost:4173/'],
+      numberOfRuns: 1,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:best-practices': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
